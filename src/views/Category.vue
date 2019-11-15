fd77ce7c051fb8d84aae281b278725ece3ecb9c5
rmad17 <souravbasu17@gmail.com>
2019-11-16 00:01:04 +0530

    Able to close modal on button click

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -4,13 +4,26 @@
         </base-header>
         <div class="container-fluid mt--7">
             <div role="tablist">
-                <b-modal id="c-edit-modal">Hello From My C Modal!</b-modal>
-                <b-modal id="t-edit-modal">Hello From My T Modal!</b-modal>
                 <b-card v-for="category in categories" :key=category.id header-border-variant="info" no-body class="mb-1">
                 <b-card-header header-tag="header" class="p-1 button-toolbar" role="tab">
-                    <b-button class="ml-4 col-sm-9" pill v-b-toggle="'category-accordion-' + category.id" variant="outline-success">{{category.name}}</b-button>
-                    <b-button class="m-2 mr-2 col-sm-1" v-b-modal.c-edit-modal><font-awesome-icon size="lg" icon="edit" :style="{color: 'gray'}"></font-awesome-icon></b-button>
-                    <b-button class="m-2 mr-2 col-sm-1"><font-awesome-icon size="lg" icon="trash-alt" :style="{color: 'gray'}"></font-awesome-icon></b-button>
+                  <b-modal :id="'c-edit-modal-' + category.id" hide-footer no-stacking hide-header-close header-bg-variant="light">
+                    <base-model-input class="input-group-alternative mb-3 d-none"
+                                        placeholder="name"
+                                        v-model="category.id">
+                    </base-model-input>
+                    <base-model-input class="input-group-alternative mb-3"
+                                        placeholder="description"
+                                        v-model="category.name">
+                    </base-model-input>
+                    <base-model-input class="input-group-alternative mb-3"
+                                        placeholder="description"
+                                        v-model="category.description">
+                    </base-model-input>
+                    <b-button class="mt-3" block pill @click="updateCategory(category)">Close Me</b-button>
+                  </b-modal>
+                  <b-button class="ml-4 col-sm-9" pill v-b-toggle="'category-accordion-' + category.id" variant="outline-success">{{category.name}}</b-button>
+                  <b-button class="m-2 mr-2 col-sm-1" v-b-modal="'c-edit-modal-' + category.id"><font-awesome-icon size="lg" icon="edit" :style="{color: 'gray'}"></font-awesome-icon></b-button>
+                  <b-button class="m-2 mr-2 col-sm-1"><font-awesome-icon size="lg" icon="trash-alt" :style="{color: 'gray'}"></font-awesome-icon></b-button>
                 </b-card-header>
                 <b-collapse :id="'category-accordion-' + category.id" accordion="category-accordion" role="tabpanel">
                     <b-card-body>
@@ -18,6 +31,7 @@
                         <div>
                           <b-card v-for='tag in category.tags' :key='tag.id' no-body class="mb-1">
                             <b-card-header header-tag="header" class="p-1" role="tab">
+                              <b-modal id="t-edit-modal">Hello From My T Modal!</b-modal>
                               <b-button class="text-left col-md-8" pill v-b-toggle="'tag-accordion-' + tag.id" variant="outline-peach">{{tag.name}}</b-button>
                               <b-button class="align-right m-2 col-sm-1" v-b-modal.t-edit-modal><font-awesome-icon size="lg" icon="edit" :style="{color: 'gray'}"></font-awesome-icon></b-button>
                               <b-button class="align-right m-2 col-sm-1"><font-awesome-icon size="lg" icon="trash-alt" :style="{color: 'gray'}"></font-awesome-icon></b-button>
@@ -47,12 +61,17 @@ export default {
   data: function () {
     return {
       showCollapse: true,
-      categories: []
+      categories: [],
+      updatedCategory: {
+        name: '',
+        description: ''
+      }
     }
   },
   computed: {
     categoryData: function () {
-      return this.$store.state.categories
+      return 1
+      // return this.$store.state.categories
     }
   },
   methods: {
@@ -64,7 +83,14 @@ export default {
     },
     storeCategory: function (responseData) {
       this.$store.dispatch('updateCategory', responseData.data)
-      this.categories = responseData.data
+      this.categories = [...responseData.data]
+    },
+    updateCategory: function (category) {
+      console.log('Hello')
+      let id = 'c-edit-modal-' + category.id
+      console.log(id)
+      this.$bvModal.hide(id)
+      // this.$id['c-edit-modal-' + category.id].hide()
     }
   },
   mounted () {
