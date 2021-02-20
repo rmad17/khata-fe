c1bf35c0e94ebc16ef34bc9cc2dfadc98bdc9719
rmad17 <souravbasu17@gmail.com>
2021-02-21 00:31:16 +0530

    Improved graph

--- src/views/Visualisation.vue
+++ src/views/Visualisation.vue
@@ -9,22 +9,16 @@
 </template>
 -->
 <script>
-import { Bar } from 'vue-chartjs'
-// import LineChart from '@/components/Charts/LineChart'
 // Charts
-// import * as chartConfigs from '@/components/Charts/config'
+import Chart from 'chart.js'
+import { Bar } from 'vue-chartjs'
+
+Chart.defaults.global.defaultFontSize = 14
+Chart.defaults.global.defaultFontColor = '#9aa8b4'
 
 export default {
   extends: Bar,
   name: 'visualisation',
-  // components: { LineChart },
-  /// props: [],
-  // data () {
-  //   return {
-  //     options: {},
-  //     chartdata: {}
-  //   }
-  // },
   data () {
     return {
       chartData: {
@@ -32,21 +26,58 @@ export default {
         datasets: [
           {
             label: 'Credit',
-            backgroundColor: '#6563d6',
+            backgroundColor: '#5ac18e',
             data: [40, 20, 35, 42, 17, 27],
-            borderWidth: 1
+            borderWidth: 1,
+            // barThickness: 20,
+            // barPercentage: 0.8,
+            categoryPercentage: 0.4,
+            pointBorderColor: 'white',
+            borderColor: 'transparent'
           },
           {
             label: 'Debit',
-            backgroundColor: '#2685c4',
+            backgroundColor: '#d0465e',
             data: [30, 30, 55, 22, 57, 7],
-            borderWidth: 1
+            borderWidth: 1,
+            // barThickness: 20,
+            // barPercentage: 0.8,
+            categoryPercentage: 0.4,
+            pointBorderColor: 'white',
+            borderColor: 'transparent'
           }
         ]
       },
       options: {
         responsive: true,
-        maintainAspectRatio: false
+        maintainAspectRatio: false,
+        scales: {
+          yAxes: [{
+            gridLines: {
+              display: true
+            }
+          }],
+          xAxes: [{
+            gridLines: {
+              display: true
+            }
+          }]
+        },
+        legend: {
+          display: true,
+          labels: {
+            fontFamily: 'sans-serif',
+            fontSize: 18
+          }
+        },
+        layout: {
+          padding: {
+            left: 50,
+            right: 50,
+            top: 0,
+            bottom: 0
+          }
+        }
       }
     }
   },
